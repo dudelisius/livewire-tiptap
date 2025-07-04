1fb8400c500a21c90bd5fc73dc0fd5eddcb99436
Rick de Graaf <dudelisius@gmail.com>
2025-07-04 11:15:16 +0200

    Fix tests

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -2,15 +2,19 @@
 
 declare(strict_types=1);
 
+if (! function_exists('arch')) {
+    return;
+}
+
 uses()->group('arch');
 
 arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
 
-arch()->preset()->php();
+arch('php preset')->preset()->php();
 
-arch()->preset()->security()
+arch('security preset')->preset()->security()
     ->ignoring('md5')
     ->ignoring('sha1')
     ->ignoring('tempnam');
